a0090ce7de6e6d8632831c0cba3b9b51264d8035
Noy <59097122+noyyyy@users.noreply.github.com>
2023-07-04 00:39:51 +0800

    feat: 🎸 change piece cor & place piece back

--- packages/contracts/src/systems/PlaceSystem.sol
+++ packages/contracts/src/systems/PlaceSystem.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
-import { Board, Player, Game, GameConfig, Piece, PieceInBattle, Creatures } from "../codegen/Tables.sol";
+import { Board, Player, Game, GameConfig, PieceData, Piece, PieceInBattle, Creatures } from "../codegen/Tables.sol";
 import { IWorld } from "src/codegen/world/IWorld.sol";
 
 import { getUniqueEntity } from "@latticexyz/world/src/modules/uniqueentity/getUniqueEntity.sol";
@@ -17,19 +17,8 @@ contract PlaceSystem is System {
   function placeToBoard(uint256 index, uint32 x, uint32 y) public returns (uint32 creatureId, uint32 tier) {
     address player = _msgSender();
 
-    // check x, y validity
-    require(x < GameConfig.getWidth(), "x too large");
-    require(y < GameConfig.getLength(), "y too large");
-
-    // check whether this place is occupied
-    {
-      uint64 cor = IWorld(_world()).encodeCor(x, y);
-      // loop piece to check whether is occupied
-      for (uint256 i = 0; i < Player.lengthPieces(player); i++) {
-        bytes32 key = Player.getItemPieces(player, i);
-        require(cor != IWorld(_world()).encodeCor(Piece.getX(key), Piece.getY(key)), "this location is not empty");
-      }
-    }
+    // check whether x,y is valid
+    checkCorValidity(player, x, y);
 
     uint64 hero = Player.getItemInventory(player, index);
     (creatureId, tier) = IWorld(_world()).decodeHero(hero);
@@ -40,23 +29,23 @@ contract PlaceSystem is System {
     // 2. pop inventory
     Player.popInventory(player);
 
+    /// @dev create piece for play
     bytes32 pieceKey = getUniqueEntity();
-    bytes32 pieceInBattleKey = getUniqueEntity();
 
     // create piece
     Piece.set(pieceKey, creatureId, uint8(tier), x, y);
     // add piece to player
     Player.pushPieces(player, pieceKey);
 
-    // create pice in battle
-    PieceInBattle.set(pieceInBattleKey, pieceKey, Creatures.getHealth(creatureId), x, y);
+    /// @notice key of piece in battle is the same as piece for a player
+    PieceInBattle.set(pieceKey, pieceKey, Creatures.getHealth(creatureId), x, y);
     // add piece in battle for player
-    Board.pushPieces(player, pieceInBattleKey);
+    Board.pushPieces(player, pieceKey);
 
+    /// @dev create piece for enemy
     {
       bytes32 pieceInBattleKeyForEnemy = getUniqueEntity();
 
-      // create piece for enemy
       PieceInBattle.set(
         pieceInBattleKeyForEnemy,
         pieceKey,
@@ -65,19 +54,7 @@ contract PlaceSystem is System {
         y
       );
 
-      // add piece in battle to enemy
-
-      uint32 gameId = Player.getGameId(player);
-
-      // get enemy addr
-      address enemyAddr;
-      if (player == Game.getPlayer1(gameId)) {
-        enemyAddr = Game.getPlayer2(gameId);
-      } else {
-        enemyAddr = Game.getPlayer1(gameId);
-      }
-
-      Board.pushPieces(enemyAddr, pieceInBattleKeyForEnemy);
+      Board.pushPieces(getEnemy(player), pieceInBattleKeyForEnemy);
     }
   }
 
@@ -86,11 +63,82 @@ contract PlaceSystem is System {
    * @param x coordinate x to place
    * @param y coordinate y to place
    */
-  function changePieceCoordinate(uint256 index, uint32 x, uint32 y) public {}
+  function changePieceCoordinate(uint256 index, uint32 x, uint32 y) public {
+    address player = _msgSender();
+    address enemy = getEnemy(player);
+
+    checkCorValidity(player, x, y);
+
+    bytes32 pieceKeyForPlayer = Player.getItemPieces(player, index);
+    bytes32 pieceKeyForEnemy = Board.getItemEnemyPieces(enemy, index);
+
+    // update piece in board
+    Piece.setX(pieceKeyForPlayer, x);
+    Piece.setY(pieceKeyForPlayer, x);
+
+    // update piece in piece in battle
+    PieceInBattle.setX(pieceKeyForPlayer, x);
+    PieceInBattle.setY(pieceKeyForPlayer, y);
+
+    // update piece in piece in battle of enemy
+    PieceInBattle.setX(pieceKeyForEnemy, GameConfig.getWidth() * 2 - 1 - x);
+    PieceInBattle.setY(pieceKeyForEnemy, y);
+  }
 
   /**
    * @dev
    * @param index index of piece in piece
    */
-  function placeBackInventory(uint256 index) public {}
+  function placeBackInventory(uint256 index) public {
+    address player = _msgSender();
+    address enemy = getEnemy(player);
+
+    bytes32 pieceKey = Player.getItemPieces(player, index);
+
+    // remove from player pieces
+    Player.updatePieces(player, index, Player.getItemPieces(player, Player.lengthPieces(player) - 1));
+    Player.popPieces(player);
+
+    // remove from pieces in battle
+    Board.updatePieces(player, index, Board.getItemPieces(player, Board.lengthPieces(player) - 1));
+    Board.popPieces(player);
+
+    // remove from pieces in battle of enemy
+    Board.updateEnemyPieces(enemy, index, Board.getItemEnemyPieces(enemy, Board.lengthEnemyPieces(enemy) - 1));
+    Board.popPieces(enemy);
+
+    /// @dev add to inventory
+
+    // check whether inventory is full
+    require(Player.lengthInventory(player) < GameConfig.getInventorySlotNum(), "inventory full");
+
+    PieceData memory pd = Piece.get(pieceKey);
+
+    Player.pushInventory(player, IWorld(_world()).encodeHero(pd.creature, pd.tier));
+  }
+
+  function checkCorValidity(address player, uint32 x, uint32 y) public view {
+    // check x, y validity
+    require(x < GameConfig.getWidth(), "x too large");
+    require(y < GameConfig.getLength(), "y too large");
+
+    // check whether (x,y) is empty
+    uint64 cor = IWorld(_world()).encodeCor(x, y);
+    // loop piece to check whether is occupied
+    for (uint256 i = 0; i < Player.lengthPieces(player); i++) {
+      bytes32 key = Player.getItemPieces(player, i);
+      require(cor != IWorld(_world()).encodeCor(Piece.getX(key), Piece.getY(key)), "this location is not empty");
+    }
+  }
+
+  function getEnemy(address player) public view returns (address enemy) {
+    // add piece in battle to enemy
+    uint32 gameId = Player.getGameId(player);
+
+    if (player == Game.getPlayer1(gameId)) {
+      enemy = Game.getPlayer2(gameId);
+    } else {
+      enemy = Game.getPlayer1(gameId);
+    }
+  }
 }
