572249bbc90cdd8b0e9e63cbb354930e03bf579b
Noy <59097122+noyyyy@users.noreply.github.com>
2023-07-05 09:40:02 +0800

    refactor: 💡 update shop conf

--- packages/contracts/script/ConfigInitializer.sol
+++ packages/contracts/script/ConfigInitializer.sol
@@ -16,28 +16,32 @@ library ConfigInitializer {
     expUpgrade[5] = 32;
     expUpgrade[6] = 48;
     expUpgrade[7] = 56;
-    
+
     GameConfig.set(
       _world,
       0, // game index
       8, // creature index
       4, // length
       8, // width
-      20,// round interval
+      20, // round interval
       0, // revenue
       0, // revenueGrowthPeriod
-      6,  // inventory slot num
+      6, // inventory slot num
       expUpgrade //expUpgrade
     );
   }
 
   function initShopConfig(IWorld _world) internal {
-    uint8[] memory tierPrice = new uint8[](2);
+    uint8[] memory tierPrice = new uint8[](3);
     tierPrice[0] = 1;
-    tierPrice[1] = 5;
-    uint8[] memory tierRate = new uint8[](2);
+    tierPrice[1] = 3;
+    tierPrice[2] = 9;
+
+    uint8[] memory tierRate = new uint8[](3);
     tierRate[0] = 80;
     tierRate[1] = 100;
+    tierRate[2] = 100;
+
     ShopConfig.set(
       _world,
       5, // slot num
@@ -52,11 +56,6 @@ library ConfigInitializer {
     uint16[] memory tierAmplifier = new uint16[](2);
     tierAmplifier[0] = 210;
     tierAmplifier[1] = 330;
-    CreatureConfig.set(
-      _world,
-      tierAmplifier,
-      tierAmplifier,
-      tierAmplifier
-    );
+    CreatureConfig.set(_world, tierAmplifier, tierAmplifier, tierAmplifier);
   }
 }
