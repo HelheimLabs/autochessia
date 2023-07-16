2e68316358b68e7c72e3062140bde8241ca6d0b3
aLIEzsss4 <vuetomato@gmail.com>
2023-07-16 23:59:06 +0800

    refactor: useChessboard

--- packages/client/src/mud/createClientComponents.ts
+++ packages/client/src/mud/createClientComponents.ts
@@ -3,6 +3,7 @@ import { SetupNetworkResult } from "./setupNetwork";
 export type ClientComponents = ReturnType<typeof createClientComponents>;
 
 export function createClientComponents({ components }: SetupNetworkResult) {
+  console.log({components})
   return {
     ...components,
     // add your client components or overrides here
