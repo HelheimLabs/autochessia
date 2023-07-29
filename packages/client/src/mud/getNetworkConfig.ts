7a0e14ed7f8044da5cb7d55d5321f5bca1bf1241
Noy <59097122+noyyyy@users.noreply.github.com>
2023-07-29 15:34:18 +0800

    chore: set lattice testnet as default

--- packages/client/src/mud/getNetworkConfig.ts
+++ packages/client/src/mud/getNetworkConfig.ts
@@ -1,6 +1,7 @@
 import { SetupContractConfig, getBurnerWallet } from "@latticexyz/std-client";
 import worldsJson from "contracts/worlds.json";
-import { altLayerTestnet, supportedChains } from "./supportedChains";
+import { supportedChains } from "./supportedChains";
+import { latticeTestnet } from "@latticexyz/common/chains";
 
 const worlds = worldsJson as Partial<
   Record<string, { address: string; blockNumber?: number }>
@@ -16,7 +17,7 @@ export async function getNetworkConfig(): Promise<NetworkConfig> {
   const params = new URLSearchParams(window.location.search);
 
   // default as lattice testnet and user can select on their own
-  const chainId = Number(params.get("chainId") || altLayerTestnet.id);
+  const chainId = Number(params.get("chainId") || latticeTestnet.id);
   const chainIndex = supportedChains.findIndex((c) => c.id === chainId);
   const chain = supportedChains[chainIndex];
   if (!chain) {
