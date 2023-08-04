f5fe8a8f2d3f6c93c5b5e3abe96ef1f0a64be936
Noy <59097122+noyyyy@users.noreply.github.com>
2023-08-05 04:13:18 +0800

    perf: cache length

--- packages/contracts/src/library/Utils.sol
+++ packages/contracts/src/library/Utils.sol
@@ -53,7 +53,7 @@ library Utils {
 
   function getFirstInventoryEmptyIdx(address _player) internal view returns (uint256) {
     uint64[] memory inv = Player.getInventory(_player);
-    uint256 length = GameConfig.getInventorySlotNum(0);
+    uint256 length = inv.length;
     for (uint256 i = 0; i < length; i++) {
       if (inv[i] == uint64(0)) {
         return i;
